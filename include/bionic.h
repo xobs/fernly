698b123bd03451a7368d3d71c7070612a4b6be81
Sean Cross <xobs@kosagi.com>
2014-11-28 13:12:07 +0800

    bionic: Remove strtoul
    We now use the one exposed in vsprintf.  This removes code duplication.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- include/bionic.h
+++ include/bionic.h
@@ -10,7 +10,7 @@ char *_strtok(char *str, const char *delim, char **saveptr);
 int _strcasecmp(const char *s1, const char *s2);
 void *_memcpy(void *dst0, const void *src0, size_t length);
 void _memset(void *dst0, char val, size_t length);
-unsigned long _strtoul(const void *nptr, void **endptr, int base);
+unsigned long strtoul(const char *nptr, char **endptr, unsigned int base);
 int _strlen(const char *s);
 int _strnlen(const char *s, uint32_t maxlen);
 void _usleep(uint32_t usecs);
